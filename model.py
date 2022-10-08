ed4a5b3f1454fc89ca7a4f3b6fd5caa2749ec347
ivyyyyy <shixiaowen.sxw@antgroup.com>
2022-10-08 11:53:59 +0800

    add to log_prob

--- model.py
+++ model.py
@@ -259,7 +259,7 @@ class BiModel(nn.Module):
         #hidden = F.relu(self.linear(emotions))
         hidden = self.dropout(hidden)
         log_prob = F.log_softmax(self.smax_fc(hidden), 2) # seq_len, batch, n_classes
-        # log_prob += self.bigfivel(bigfive)
+        log_prob = log_prob + self.bigfivel(bigfive)
         loss = -self.crf(log_prob, label, umask.T.byte())
 
         if att2:
